034fb63e5e48db419af63cb2473225f510de7ca0
urukgit <uruk@scummvm.org>
2015-03-31 02:24:10 +0200

    Some safeguard check when adding a new friend.

--- WinTox/ViewModel/MainViewModel.cs
+++ WinTox/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using SharpTox.Core;
 using WinTox.Common;
@@ -17,10 +18,20 @@ namespace WinTox.ViewModel {
                     ?? (_addFriendCommand = new RelayCommand(
                     (object parameter) => {
                         var content = (StackPanel)parameter;
-                        var friendID = ((TextBox)content.FindName("FriendID")).Text;
+
+                        var friendIdTextBox = (TextBox)content.FindName("FriendID");
+                        var friendId = friendIdTextBox.Text;
+                        if (friendId == String.Empty) {
+                            friendIdTextBox.Focus(FocusState.Programmatic);
+                            return;
+                        }
+
                         var invitationMessage = ((TextBox)content.FindName("InvitationMessage")).Text;
+                        if (invitationMessage == String.Empty)
+                            invitationMessage = "Hello! I'd like to add you to my friends list.";
+
                         ToxErrorFriendAdd error;
-                        ToxViewModel.Instance.AddFriend(new ToxId(friendID), invitationMessage, out error);
+                        ToxViewModel.Instance.AddFriend(new ToxId(friendId), invitationMessage, out error);
                         // TODO: Handle errors!!!
                     }));
             }
