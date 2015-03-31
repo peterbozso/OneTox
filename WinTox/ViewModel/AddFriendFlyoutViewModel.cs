76ae9d4e1f687f4bad2313c9662e7ed4e59d98ef
urukgit <uruk@scummvm.org>
2015-03-31 20:22:55 +0200

    Split up DP-s and and ViewModel of AddFriendFlyout.
    Hopefully it's less confusing this way. (At least for me it is.)

--- WinTox/ViewModel/AddFriendFlyoutViewModel.cs
+++ WinTox/ViewModel/AddFriendFlyoutViewModel.cs
@@ -12,8 +12,6 @@ using WinTox.Common;
 
 namespace WinTox.ViewModel {
     class AddFriendFlyoutViewModel : INotifyPropertyChanged {
-        #region Properties
-
         private bool _isFlyoutOpen;
 
         public bool IsFlyoutOpen {
@@ -53,76 +51,11 @@ namespace WinTox.ViewModel {
             }
         }
 
-        #endregion
-
-        #region PropertyChanged
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
-
-        #endregion
-
-        #region DependencyProperty
-
-        // From: https://marcominerva.wordpress.com/2013/07/30/using-windows-8-1-flyout-xaml-control-with-mvvm/
-
-        public static readonly DependencyProperty IsOpenProperty =
-            DependencyProperty.RegisterAttached("IsOpen", typeof(bool),
-                typeof(AddFriendFlyoutViewModel), new PropertyMetadata(false, OnIsOpenPropertyChanged));
-
-        public static readonly DependencyProperty ParentProperty =
-            DependencyProperty.RegisterAttached("Parent", typeof(Button),
-                typeof(AddFriendFlyoutViewModel), new PropertyMetadata(null, OnParentPropertyChanged));
-
-        public static void SetIsOpen(DependencyObject d, bool value) {
-            d.SetValue(IsOpenProperty, value);
-        }
-
-        public static bool GetIsOpen(DependencyObject d) {
-            return (bool)d.GetValue(IsOpenProperty);
-        }
-
-        public static void SetParent(DependencyObject d, Button value) {
-            d.SetValue(ParentProperty, value);
-        }
-
-        public static Button GetParent(DependencyObject d) {
-            return (Button)d.GetValue(ParentProperty);
-        }
-
-        private static void OnParentPropertyChanged(DependencyObject d,
-            DependencyPropertyChangedEventArgs e) {
-            var flyout = d as Flyout;
-            if (flyout != null) {
-                flyout.Opening += (s, args) => {
-                    flyout.SetValue(IsOpenProperty, true);
-                };
-
-                flyout.Closed += (s, args) => {
-                    flyout.SetValue(IsOpenProperty, false);
-                };
-            }
-        }
-
-        private static void OnIsOpenPropertyChanged(DependencyObject d,
-            DependencyPropertyChangedEventArgs e) {
-            var flyout = d as Flyout;
-            var parent = (Button)d.GetValue(ParentProperty);
-
-            if (flyout != null && parent != null) {
-                var newValue = (bool)e.NewValue;
-
-                if (newValue)
-                    flyout.ShowAt(parent);
-                else
-                    flyout.Hide();
-            }
-        }
-
-        #endregion  
     }
 }
