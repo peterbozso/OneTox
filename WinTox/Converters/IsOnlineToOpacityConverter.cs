5b90d957dd5b984dd89549fb31a8017e58a69dcf
urukgit <uruk@scummvm.org>
2015-04-02 17:11:45 +0200

    Properly indicate if friends are offline.

---
+++ WinTox/Converters/IsOnlineToOpacityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace WinTox.Converters
+{
+    internal class IsOnlineToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var isOnline = (bool)value;
+            if (isOnline)
+                return 1;
+            else
+                return 0.5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
---
+++ WinTox/Converters/IsOnlineToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace WinTox.Converters
+{
+    internal class IsOnlineToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var isOnline = (bool)value;
+            if (isOnline)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
