b17a0fdc64cba88196735c649198c315e2832b11
urukgit <uruk@scummvm.org>
2015-04-02 14:00:03 +0200

    Introduce FirendInfo user control and make use of it.

---
+++ WinTox/View/UserControls/FriendInfo.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace WinTox.View.UserControls
+{
+    public sealed partial class FriendInfo : UserControl
+    {
+        public FriendInfo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
