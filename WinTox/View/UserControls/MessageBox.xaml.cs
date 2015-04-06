cbf56ed7050709a729c7bdebf2d60f932b81b416
urukgit <uruk@scummvm.org>
2015-04-06 06:31:07 +0200

    Add MessageBox usercontrol.

---
+++ WinTox/View/UserControls/MessageBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace WinTox.View.UserControls
+{
+    public sealed partial class MessageBox : UserControl
+    {
+        public MessageBox()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
