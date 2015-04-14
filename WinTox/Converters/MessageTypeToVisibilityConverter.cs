a40bed3aec905e4b4bf61485e98c55a18747f2ec
urukgit <uruk@scummvm.org>
2015-04-14 20:44:30 +0200

    Redesign display of action messages.

---
+++ WinTox/Converters/MessageTypeToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using SharpTox.Core;
+
+namespace WinTox.Converters
+{
+    class MessageTypeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var messageType = (ToxMessageType)value;
+            switch (messageType)
+            {
+                case ToxMessageType.Message:
+                    return Visibility.Visible;
+                case ToxMessageType.Action:
+                    return Visibility.Collapsed;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
