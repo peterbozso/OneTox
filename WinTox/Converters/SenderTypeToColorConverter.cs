987a0b81d94ab275a0ef826d32432395fdccf92f
urukgit <uruk@scummvm.org>
2015-04-18 21:13:36 +0200

    Reimplement message background coloring.

--- WinTox/Converters/SenderTypeToColorConverter.cs
+++ WinTox/Converters/SenderTypeToColorConverter.cs
@@ -10,17 +10,12 @@ namespace WinTox.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            /*
-            var senderType = (MessageViewModel.MessageSenderType) value;
-            switch (senderType)
-            {
-                case MessageViewModel.MessageSenderType.Friend:
-                    return Application.Current.Resources["TextColor"] as SolidColorBrush;
+            if (value is FriendViewModel)
+                return Application.Current.Resources["TextColor"] as SolidColorBrush;
 
-                case MessageViewModel.MessageSenderType.User:
-                    return Application.Current.Resources["ChatBackgroundColor"] as SolidColorBrush;
-            }
-            */
+            if (value is UserViewModel)
+                return Application.Current.Resources["ChatBackgroundColor"] as SolidColorBrush;
+            
             return null;
         }
 
