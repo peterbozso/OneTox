91cd2fcb2d52a2114717ca2dc4b6c2ae0e158f48
urukgit <uruk@scummvm.org>
2015-05-28 13:58:04 +0200

    Start implementation of FileTransferManager.

--- WinTox/Model/FileTransferManager.cs
+++ WinTox/Model/FileTransferManager.cs
@@ -1,4 +1,11 @@
-﻿namespace WinTox.Model
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using SharpTox.Core;
+using System.Threading.Tasks;
+
+namespace WinTox.Model
 {
     /// <summary>
     ///     Implements the Singleton pattern. (https://msdn.microsoft.com/en-us/library/ff650849.aspx)
@@ -7,8 +14,38 @@
     {
         private static FileTransferManager _instance;
 
+        private readonly Dictionary<ToxFileInfo, IRandomAccessStream> _filesBeingSent; 
+
         private FileTransferManager()
         {
+            _filesBeingSent = new Dictionary<ToxFileInfo, IRandomAccessStream>();
+            ToxModel.Instance.FileControlReceived += FileControlReceivedHandler;
+            ToxModel.Instance.FileChunkRequested += FileChunkRequestedHandler;
+        }
+
+        private void FileControlReceivedHandler(object sender, ToxEventArgs.FileControlEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FileChunkRequestedHandler(object sender, ToxEventArgs.FileRequestChunkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Send(int friendNumber, ToxFileKind kind,  StorageFile file)
+        {
+            var stream = await file.OpenReadAsync();
+            ToxErrorFileSend error;
+            var fileInfo = ToxModel.Instance.FileSend(friendNumber, kind, (long) stream.Size, file.Name, out error);
+            if (error == ToxErrorFileSend.Ok)
+            {
+                _filesBeingSent.Add(fileInfo, stream);
+            }
+            else
+            {
+                // TODO: Handle error!
+            }
         }
 
         public static FileTransferManager Instance
