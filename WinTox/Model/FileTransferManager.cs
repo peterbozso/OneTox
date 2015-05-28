2b90a10a70e23fbf9d770a588c7b3f823caff51b
urukgit <uruk@scummvm.org>
2015-05-28 11:24:17 +0200

    Add FileTransferManager stub.

---
+++ WinTox/Model/FileTransferManager.cs
@@ -0,0 +1,19 @@
+﻿namespace WinTox.Model
+{
+    /// <summary>
+    ///     Implements the Singleton pattern. (https://msdn.microsoft.com/en-us/library/ff650849.aspx)
+    /// </summary>
+    public class FileTransferManager
+    {
+        private static FileTransferManager _instance;
+
+        private FileTransferManager()
+        {
+        }
+
+        public static FileTransferManager Instance
+        {
+            get { return _instance ?? (_instance = new FileTransferManager()); }
+        }
+    }
+}
