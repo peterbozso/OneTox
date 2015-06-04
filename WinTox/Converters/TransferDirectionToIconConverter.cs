582d06c7b40ddff6074d89b4aeb8b997a46a83b3
urukgit <uruk@scummvm.org>
2015-06-04 12:00:26 +0200

    Implement icon indicator of direction for file transfers.

---
+++ WinTox/Converters/TransferDirectionToIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml.Data;
+using WinTox.ViewModel.FileTransfer;
+
+namespace WinTox.Converters
+{
+    class TransferDirectionToIconConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, string language)
+        {
+            var direction = (FileTransferDirection) value;
+            switch (direction)
+            {
+                case FileTransferDirection.Down:
+                    return "";
+                case FileTransferDirection.Up:
+                    return "";
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
