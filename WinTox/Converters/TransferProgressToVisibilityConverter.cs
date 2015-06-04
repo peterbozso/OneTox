9a8870dff619d8f260604b491c5a62bb159b9ac2
urukgit <uruk@scummvm.org>
2015-06-04 14:05:37 +0200

    Hide the Pause/Resume button if the file transfer is finished.

---
+++ WinTox/Converters/TransferProgressToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace WinTox.Converters
+{
+    internal class TransferProgressToVisibilityConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            var isTransferFinished = ((Double) value).Equals(100.0);
+            if (isTransferFinished)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
