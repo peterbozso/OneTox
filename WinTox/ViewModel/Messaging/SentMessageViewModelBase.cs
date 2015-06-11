414e4dfb536666b03b0cb2b79702f0858c89eb41
urukgit <uruk@scummvm.org>
2015-06-11 19:38:37 +0200

    Implement automatic resend of unreceived messages.

--- WinTox/ViewModel/Messaging/SentMessageViewModelBase.cs
+++ WinTox/ViewModel/Messaging/SentMessageViewModelBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using SharpTox.Core;
@@ -11,6 +12,8 @@ namespace WinTox.ViewModel.Messaging
     {
         private readonly CoreDispatcher _dispatcher;
         private readonly FriendViewModel _target;
+        private Timer _resendTimer;
+        private int _timerCallbackFired;
 
         public SentMessageViewModelBase(string text, DateTime timestamp, ToxMessageType messageType, int id,
             FriendViewModel target)
@@ -27,9 +30,40 @@ namespace WinTox.ViewModel.Messaging
 
             ToxModel.Instance.ReadReceiptReceived += ReadReceiptReceivedHandler;
             ToxModel.Instance.FriendConnectionStatusChanged += FriendConnectionStatusChangedHandler;
+
+            SetupAndStartResendTimer();
         }
 
-        public int Id { get; set; }
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// We automatically resend the message every 10 seconds for 3 times if we don't get a read receipt
+        /// for it in 10 seconds after we sent it for the first time.
+        /// </summary>
+        private void SetupAndStartResendTimer()
+        {
+            if (ToxModel.Instance.IsFriendOnline(_target.FriendNumber))
+            {
+                _timerCallbackFired = 0;
+
+                _resendTimer = new Timer(
+                    state =>
+                    {
+                        if (IsDelivered) // If it's delivered, then there's no need for resend.
+                        {
+                            _resendTimer.Dispose();
+                            return;
+                        }
+
+                        ResendMessage();
+
+                        _timerCallbackFired++;
+                        if (_timerCallbackFired == 3) // Don't resend it automatically more than 3 times.
+                            _resendTimer.Dispose();
+                    },
+                    null, 10000, 10000);
+            }
+        }
 
         private async void ReadReceiptReceivedHandler(object sender, ToxEventArgs.ReadReceiptEventArgs e)
         {
@@ -47,16 +81,14 @@ namespace WinTox.ViewModel.Messaging
             if (e.FriendNumber != _target.FriendNumber)
                 return;
 
-            ResendMessage();
+            if (!IsDelivered)
+                ResendMessage();
         }
 
         private void ResendMessage()
         {
-            if (!IsDelivered)
-            {
-                var messageId = ToxModel.Instance.SendMessage(_target.FriendNumber, Text, MessageType);
-                Id = messageId; // We have to update the message ID.
-            }
+            var messageId = ToxModel.Instance.SendMessage(_target.FriendNumber, Text, MessageType);
+            Id = messageId; // We have to update the message ID.
         }
     }
 }
