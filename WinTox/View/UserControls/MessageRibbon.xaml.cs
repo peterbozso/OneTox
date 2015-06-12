8a13e8b95b89b2c6ad4df7c6c0c7b9d6c503b42e
urukgit <uruk@scummvm.org>
2015-06-12 10:02:04 +0200

    Introduce MessageRibbon.

---
+++ WinTox/View/UserControls/MessageRibbon.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace WinTox.View.UserControls
+{
+    public sealed partial class MessageRibbon : UserControl
+    {
+        public MessageRibbon()
+        {
+            InitializeComponent();
+        }
+    }
+}
