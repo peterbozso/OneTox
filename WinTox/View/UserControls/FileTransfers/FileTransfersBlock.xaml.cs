ab538018db13a5a34985f330ba1a8587af2832a0
urukgit <uruk@scummvm.org>
2015-06-22 14:06:56 +0200

    Simplify transition animations on FileTransfersBlock.

--- WinTox/View/UserControls/FileTransfers/FileTransfersBlock.xaml.cs
+++ WinTox/View/UserControls/FileTransfers/FileTransfersBlock.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System.ComponentModel;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using WinTox.ViewModel.FileTransfers;
@@ -12,9 +13,6 @@ namespace WinTox.View.UserControls.FileTransfers
         public FileTransfersBlock()
         {
             InitializeComponent();
-
-            HidePlaceholderStoryboard.Completed += HidePlaceholderStoryboardCompleted;
-            HideTransfersStoryboard.Completed += HideTransfersStoryboardCompleted;
         }
 
         private void FileTransferBlockLoaded(object sender, RoutedEventArgs e)
@@ -24,41 +22,21 @@ namespace WinTox.View.UserControls.FileTransfers
             _viewModel.VisualStates.PropertyChanged += VisualStatesPropertyChangedHandler;
         }
 
-        private void VisualStatesPropertyChangedHandler(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private void VisualStatesPropertyChangedHandler(object sender, PropertyChangedEventArgs e)
         {
             if (e.PropertyName == "OpenContentGridHeight")
                 OpenContentGrid.Height = _viewModel.VisualStates.OpenContentGridHeight;
         }
 
-        #region Show arrow tap
-
         private void ShowArrowTextBlockTapped(object sender, TappedRoutedEventArgs e)
         {
-            HidePlaceholderStoryboard.Begin();
-        }
-
-        private void HidePlaceholderStoryboardCompleted(object sender, object e)
-        {
             _viewModel.VisualStates.BlockState = FileTransfersViewModel.VisualStatesViewModel.TransfersBlockState.Open;
-            ShowTransfersStoryboard.Begin();
         }
-
-        #endregion
-
-        #region Hide arrow tap
 
         private void HideArrowTextBlockTapped(object sender, TappedRoutedEventArgs e)
         {
-            HideTransfersStoryboard.Begin();
-        }
-
-        private void HideTransfersStoryboardCompleted(object sender, object e)
-        {
             _viewModel.VisualStates.BlockState =
                 FileTransfersViewModel.VisualStatesViewModel.TransfersBlockState.Collapsed;
-            ShowPlaceholderStoryboard.Begin();
         }
-
-        #endregion
     }
 }
