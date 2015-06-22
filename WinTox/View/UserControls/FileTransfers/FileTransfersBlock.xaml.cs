cdfece133a17dc2cb1fc54273e22d998b5db638b
urukgit <uruk@scummvm.org>
2015-06-22 16:46:44 +0200

    Add add/delete transition animation for file transfers.

--- WinTox/View/UserControls/FileTransfers/FileTransfersBlock.xaml.cs
+++ WinTox/View/UserControls/FileTransfers/FileTransfersBlock.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media.Animation;
 using WinTox.ViewModel.FileTransfers;
 
 namespace WinTox.View.UserControls.FileTransfers
@@ -15,11 +17,20 @@ namespace WinTox.View.UserControls.FileTransfers
             InitializeComponent();
         }
 
-        private void FileTransferBlockLoaded(object sender, RoutedEventArgs e)
+        private async void FileTransferBlockLoaded(object sender, RoutedEventArgs e)
         {
             _viewModel = DataContext as FileTransfersViewModel;
             VisualStateManager.GoToState(this, _viewModel.VisualStates.BlockState.ToString(), true);
             _viewModel.VisualStates.PropertyChanged += VisualStatesPropertyChangedHandler;
+            await SetAddDeleteThemeTransitionForTransferRibbons();
+        }
+
+        private async Task SetAddDeleteThemeTransitionForTransferRibbons()
+        {
+            // We need this ugly hack becouse otherwise everytime we navigate to ChatPage
+            // we'd see the "Add" animation of every item in the list (and we do not want that).
+            await Task.Delay(1);
+            TransferRibbons.ItemContainerTransitions = new TransitionCollection {new AddDeleteThemeTransition()};
         }
 
         private void VisualStatesPropertyChangedHandler(object sender, PropertyChangedEventArgs e)
