cb030d807b57c574ecd950d072aed0a9efd7b64a
urukgit <uruk@scummvm.org>
2015-06-23 13:06:57 +0200

    Show the scrollbar when there are too many file transfers.

--- WinTox/ViewModel/FileTransfers/FileTransfersViewModel.cs
+++ WinTox/ViewModel/FileTransfers/FileTransfersViewModel.cs
@@ -181,7 +181,7 @@ namespace WinTox.ViewModel.FileTransfers
                 get { return _openContentGridHeight; }
                 private set
                 {
-                    _openContentGridHeight = value > 240 ? 270 : value;
+                    _openContentGridHeight = value > 240 ? 240 : value;
                     RaisePropertyChanged();
                 }
             }
