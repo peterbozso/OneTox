efc18ef9dc008a0ca0c6c4db60f7dde6a46637d8
urukgit <uruk@scummvm.org>
2015-07-13 17:22:42 +0200

    Change visibility of HubSections based on count of items to display in them.

--- WinTox/ViewModel/MainPageViewModel.cs
+++ WinTox/ViewModel/MainPageViewModel.cs
@@ -1,14 +1,70 @@
-﻿using WinTox.ViewModel.Friends;
+﻿using System.Collections.Specialized;
+using Windows.UI.Xaml;
+using WinTox.ViewModel.FriendRequests;
+using WinTox.ViewModel.Friends;
+using WinTox.ViewModel.Messaging.RecentMessages;
 
 namespace WinTox.ViewModel
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : ViewModelBase
     {
+        private Visibility _friendRequestsListVisibility;
+        private Visibility _recentMessagesListVisibility;
+
         public MainPageViewModel()
         {
             FriendList = new FriendListViewModel();
+            DecideFriendRequestsListVisibility();
+            DecideRecentMessagesListVisiblity();
+            RecentMessagesGlobalViewModel.Instace.RecentMessages.CollectionChanged +=
+                RecentMessagesCollectionChangedHandler;
+            FriendRequestsViewModel.Instance.FriendRequests.CollectionChanged += FriendRequestsCollectionChangedHandler;
         }
 
         public FriendListViewModel FriendList { get; set; }
+
+        public Visibility RecentMessagesListVisibility
+        {
+            get { return _recentMessagesListVisibility; }
+            set
+            {
+                _recentMessagesListVisibility = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Visibility FriendRequestsListVisibility
+        {
+            get { return _friendRequestsListVisibility; }
+            set
+            {
+                _friendRequestsListVisibility = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void FriendRequestsCollectionChangedHandler(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            DecideFriendRequestsListVisibility();
+        }
+
+        private void DecideFriendRequestsListVisibility()
+        {
+            FriendRequestsListVisibility = FriendRequestsViewModel.Instance.FriendRequests.Count > 0
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        private void RecentMessagesCollectionChangedHandler(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            DecideRecentMessagesListVisiblity();
+        }
+
+        private void DecideRecentMessagesListVisiblity()
+        {
+            RecentMessagesListVisibility = RecentMessagesGlobalViewModel.Instace.RecentMessages.Count > 0
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
     }
 }
