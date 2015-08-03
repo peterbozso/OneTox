e14fc09da80397d59fe0aa8c98f38b8bb2f61e0f
Peter Bozsó <uruk@scummvm.org>
2015-08-03 22:35:53 +0200

    Start reimplementation of audio frame sending.

--- OneTox/ViewModel/Calls/AudioCallViewModel.cs
+++ OneTox/ViewModel/Calls/AudioCallViewModel.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Windows.Media;
+using Windows.Media.Audio;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Media.Render;
 using OneTox.Common;
 using OneTox.Helpers;
+using OneTox.Model;
+using SharpTox.Av;
 
 namespace OneTox.ViewModel.Calls
 {
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
     public class AudioCallViewModel : ObservableObject
     {
         public enum CallState
@@ -14,14 +31,112 @@ namespace OneTox.ViewModel.Calls
             IncomingCall
         }
 
-        private int _friendNumber;
-
         public AudioCallViewModel(int friendNumber)
         {
             _friendNumber = friendNumber;
+            
+            InitializeRates();
+        }
+
+        private void InitializeRates()
+        {
+            _bitRate = 48;
+            _samplingRate = 48*1000;
         }
 
         public event EventHandler<string> MicrophoneIsNotAvailable;
+
+        #region Fields
+
+        private readonly int _friendNumber;
+        private AudioGraph _audioGraph;
+        private AudioDeviceInputNode _deviceInputNode;
+        private AudioFrameOutputNode _frameOutputNode;
+        private int _samplingRate;
+        private int _bitRate;
+
+        #endregion
+
+        #region Audio sending
+
+        private async Task StartAudioGraph()
+        {
+            await InitAudioGraph();
+            await CreateDeviceInputNode();
+            CreateFrameOutputNode();
+            _audioGraph.Start();
+        }
+
+        private async Task InitAudioGraph()
+        {
+            var encodingProperties = AudioEncodingProperties.CreatePcm((uint) _samplingRate, 1, 16);
+
+            var settings = new AudioGraphSettings(AudioRenderCategory.Communications)
+            {
+                EncodingProperties = encodingProperties
+            };
+
+            var result = await AudioGraph.CreateAsync(settings);
+            if (result.Status != AudioGraphCreationStatus.Success)
+            {
+                // TODO: Error handling!
+                // ShowErrorMessage("AudioGraph creation error: " + result.Status.ToString());
+                return;
+            }
+
+            _audioGraph = result.Graph;
+        }
+
+        private async Task CreateDeviceInputNode()
+        {
+            // Create a device output node
+            var result = await _audioGraph.CreateDeviceInputNodeAsync(MediaCategory.Communications);
+
+            if (result.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                // TODO: Error handling!
+                // Cannot create device output node
+                // ShowErrorMessage(result.Status.ToString());
+                return;
+            }
+
+            _deviceInputNode = result.DeviceInputNode;
+        }
+
+        private void CreateFrameOutputNode()
+        {
+            _frameOutputNode = _audioGraph.CreateFrameOutputNode();
+            _audioGraph.QuantumProcessed += AudioGraphQuantumProcessed;
+            _deviceInputNode.AddOutgoingConnection(_frameOutputNode);
+        }
+
+        private void AudioGraphQuantumProcessed(AudioGraph sender, object args)
+        {
+            var frame = _frameOutputNode.GetFrame();
+            ProcessFrameOutput(frame);
+        }
+
+        private unsafe void ProcessFrameOutput(AudioFrame frame)
+        {
+            using (var buffer = frame.LockBuffer(AudioBufferAccessMode.Read))
+            using (var reference = buffer.CreateReference())
+            {
+                byte* dataInBytes;
+                uint capacityInBytes;
+
+                ((IMemoryBufferByteAccess) reference).GetBuffer(out dataInBytes, out capacityInBytes);
+
+                var bytes = new byte[capacityInBytes];
+                Marshal.Copy((IntPtr) dataInBytes, bytes, 0, (int) capacityInBytes);
+
+                var shorts = new short[capacityInBytes/2];
+                Buffer.BlockCopy(bytes, 0, shorts, 0, (int) capacityInBytes);
+
+                ToxAvModel.Instance.SendAudioFrame(_friendNumber, new ToxAvAudioFrame(shorts, _samplingRate, 1));
+            }
+        }
+
+        #endregion
 
         #region Constants
 
@@ -131,7 +246,11 @@ namespace OneTox.ViewModel.Calls
             get
             {
                 return _startCallCommand ??
-                       (_startCallCommand = new RelayCommand(() => { }));
+                       (_startCallCommand = new RelayCommand(async () =>
+                       {
+                           ToxAvModel.Instance.Call(_friendNumber, _bitRate, 0);
+                           await StartAudioGraph();
+                       }));
             }
         }
 
