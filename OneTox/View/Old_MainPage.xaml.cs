3d281cbc861efee814d922c856110504ae37dceb
Peter Bozsó <uruk@scummvm.org>
2015-08-07 12:52:00 +0200

    Start redesigning the UI with MainPage and FriendList.
    Also add some sample data for design time.

---
+++ OneTox/View/Old_MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using OneTox.Common;
+using OneTox.ViewModel;
+
+namespace OneTox.View
+{
+    /// <summary>
+    ///     A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class Old_MainPage : Page
+    {
+        private readonly MainPageViewModel _viewModel;
+
+        public Old_MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
+            NavigationHelper = new NavigationHelper(this);
+            NavigationHelper.LoadState += navigationHelper_LoadState;
+            _viewModel = DataContext as MainPageViewModel;
+
+            SizeChanged += MainPageSizeChanged;
+        }
+
+        /// <summary>
+        ///     NavigationHelper is used on each page to aid in navigation and
+        ///     process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper { get; }
+
+        private void MainPageSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, e.NewSize.Width < 700 ? "MinimalLayout" : "DefaultLayout", true);
+        }
+
+        /// <summary>
+        ///     Populates the page with content passed during navigation.  Any saved state is also
+        ///     provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        ///     The source of the event; typically <see cref="NavigationHelper" />
+        /// </param>
+        /// <param name="e">
+        ///     Event data that provides both the navigation parameter passed to
+        ///     <see cref="Frame.Navigate(Type, Object)" /> when this page was initially requested and
+        ///     a dictionary of state preserved by this page during an earlier
+        ///     session.  The state will be null the first time a page is visited.
+        /// </param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the
+        /// <see cref="Common.NavigationHelper.LoadState" />
+        /// and
+        /// <see cref="Common.NavigationHelper.SaveState" />
+        /// .
+        /// The navigation parameter is available in the LoadState method
+        /// in addition to page state preserved during an earlier session.
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            NavigationHelper.OnNavigatedTo(e);
+
+            if (e.NavigationMode == NavigationMode.New)
+                await _viewModel.FriendRequests.RestoreData();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            NavigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
---
+++ OneTox/View/UserControls/FriendListItem.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace OneTox.View.UserControls
+{
+    public sealed partial class FriendListItem : UserControl
+    {
+        public FriendListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
