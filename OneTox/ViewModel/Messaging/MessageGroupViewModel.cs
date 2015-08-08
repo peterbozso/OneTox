4ccdf75eab41400c2d8258864049d3327a798151
Peter Bozsó <uruk@scummvm.org>
2015-08-08 08:53:29 +0200

    Add back the timestamp display.

--- OneTox/ViewModel/Messaging/MessageGroupViewModel.cs
+++ OneTox/ViewModel/Messaging/MessageGroupViewModel.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using OneTox.Helpers;
 
 namespace OneTox.ViewModel.Messaging
 {
-    public class MessageGroupViewModel
+    public class MessageGroupViewModel : ObservableObject
     {
+        private DateTime _timestamp;
+
         public MessageGroupViewModel(IToxUserViewModel sender)
         {
             Sender = sender;
             Messages = new ObservableCollection<ToxMessageViewModelBase>();
+            Messages.CollectionChanged += MessagesCollectionChangedHandler;
         }
 
-        public IToxUserViewModel Sender { get; private set; }
-        public ObservableCollection<ToxMessageViewModelBase> Messages { get; private set; }
+        public IToxUserViewModel Sender { get; }
+        public ObservableCollection<ToxMessageViewModelBase> Messages { get; }
+
+        public DateTime Timestamp
+        {
+            get { return _timestamp; }
+            private set
+            {
+                if (value == _timestamp)
+                    return;
+                _timestamp = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void MessagesCollectionChangedHandler(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Timestamp = ((ToxMessageViewModelBase) e.NewItems[e.NewItems.Count - 1]).Timestamp;
+        }
     }
 }
