453369f9e4a4d64ee139b9eb0e7e9020ba39d9f6
Peter Bozsó <uruk@scummvm.org>
2015-08-08 12:17:36 +0200

    Reimplement friend request sending UI.

---
+++ OneTox/View/UserControls/Friends/AddFriendButton.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace OneTox.View.UserControls.FriendRequests
+{
+    public sealed partial class AddFriendButton : UserControl
+    {
+        public AddFriendButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
