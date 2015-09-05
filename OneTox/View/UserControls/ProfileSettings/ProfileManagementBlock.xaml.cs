d1158b905cee718febffd9db380ad3ab88057e6c
Peter Bozsó <uruk@scummvm.org>
2015-09-05 17:28:52 +0200

    Some simplification on ProfileManagementBlock.

--- OneTox/View/UserControls/ProfileSettings/ProfileManagementBlock.xaml.cs
+++ OneTox/View/UserControls/ProfileSettings/ProfileManagementBlock.xaml.cs
@@ -6,17 +6,14 @@ namespace OneTox.View.UserControls.ProfileSettings
 {
     public sealed partial class ProfileManagementBlock : UserControl
     {
-        private readonly ProfileManagementViewModel _viewModel;
-
         public ProfileManagementBlock()
         {
             InitializeComponent();
-            _viewModel = DataContext as ProfileManagementViewModel;
         }
 
         private async void ProfileManagementBlockLoaded(object sender, RoutedEventArgs e)
         {
-            await _viewModel.RefreshProfileList();
+            await (DataContext as ProfileManagementViewModel).RefreshProfileList();
         }
     }
 }
