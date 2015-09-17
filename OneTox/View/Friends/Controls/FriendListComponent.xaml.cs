2a65afdcf7dcbeedf33670d6a1130d858fd10089
Peter Bozsó <uruk@scummvm.org>
2015-09-17 17:59:23 +0200

    Introduce FriendListComponent.

---
+++ OneTox/View/Friends/Controls/FriendListComponent.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OneTox.View.Friends.Controls
+{
+    public sealed partial class FriendListComponent : UserControl
+    {
+        public static readonly DependencyProperty SelectionModeProperty = DependencyProperty.Register(
+            "SelectionMode", typeof (ListViewSelectionMode), typeof (FriendListComponent), null);
+
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(
+            "SelectedItem", typeof (object), typeof (FriendListComponent), null);
+
+        public FriendListComponent()
+        {
+            InitializeComponent();
+        }
+
+        public object SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public ListViewSelectionMode SelectionMode
+        {
+            get { return (ListViewSelectionMode) GetValue(SelectionModeProperty); }
+            set { SetValue(SelectionModeProperty, value); }
+        }
+
+        public event ItemClickEventHandler ItemClick;
+
+        private void ListViewItemClick(object sender, ItemClickEventArgs e)
+        {
+            ItemClick?.Invoke(this, e);
+        }
+    }
+}
