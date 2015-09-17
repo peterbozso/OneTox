d9baab98a48e8d13e81bd3c35840972770ff8182
Peter Bozsó <uruk@scummvm.org>
2015-09-17 18:16:29 +0200

    Introduce NavigationBar.

---
+++ OneTox/View/NavigationBar.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OneTox.View
+{
+    public sealed partial class NavigationBar : UserControl
+    {
+        public NavigationBar()
+        {
+            InitializeComponent();
+        }
+
+        public event RoutedEventHandler AddFriendClick;
+        public event RoutedEventHandler SettingsClick;
+
+        private void AddFriendButtonClick(object sender, RoutedEventArgs e)
+        {
+            AddFriendClick?.Invoke(this, e);
+        }
+
+        private void SettingsButtonClick(object sender, RoutedEventArgs e)
+        {
+            SettingsClick?.Invoke(this, e);
+        }
+    }
+}
