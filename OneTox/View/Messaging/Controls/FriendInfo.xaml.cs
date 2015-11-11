6b8c1f74c4c256fdac80659644695b11dd7923f8
Peter Bozsó <uruk@scummvm.org>
2015-11-11 09:16:08 +0100

    Introduce FriendInfo control.

---
+++ OneTox/View/Messaging/Controls/FriendInfo.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace OneTox.View.Messaging.Controls
+{
+    public sealed partial class FriendInfo : UserControl
+    {
+        public FriendInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
